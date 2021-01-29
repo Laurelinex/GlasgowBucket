8eeb86c82086878a49e02d8a41ac0560fbe048a0
codeclan student <codeclanstudent@codeclans-MacBook-Pro.local>
2021-01-29 14:51:32 +0000

    Initial Commit - db created

---
+++ db/glasgow_bucket_list.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS zones;
+
+
+CREATE TABLE zones (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE locations (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    category VARCHAR(255),
+    description TEXT,
+    visited BOOLEAN,
+    zone_id INT REFERENCES zones(id)
+);
